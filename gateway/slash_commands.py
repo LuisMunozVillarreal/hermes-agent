Carrie111998/Hermes

feat(delegation): bounded resource-aware background admission queue
Queue async delegations when child-slot capacity or memory resources
are unavailable instead of falling back to synchronous execution.
FIFO admission, per-child slot reservation, configurable queue length
and timeout, memory/PSI gating with hysteresis, queued interruption,
graceful stop cleanup, durable queued records, and recovery. Includes
config defaults, docs, and regression tests.

gateway/slash_commands.py
@@ -1365,7 +1365,7 @@ class GatewaySlashCommandsMixin:
             from tools.async_delegation import list_async_delegations
             delegations = [
                 d for d in list_async_delegations()
-                if d.get("status") in ("running", "stalling", "finalizing")
+                if d.get("status") in ("queued", "running", "stalling", "finalizing")
             ]
         except Exception:
             delegations = []
@@ -1385,7 +1385,9 @@ class GatewaySlashCommandsMixin:
                     goal = goal[:67] + "..."
                 status = d.get("status", "?")
                 row = f"- `{d.get('delegation_id', '?')}` · {status}"
-                if status == "stalling":
+                if status == "queued":
+                    row += f" · {d.get('queue_reason', 'capacity')}"
+                elif status == "stalling":
                     quiet = d.get("stalled_after_quiet_seconds")
                     if quiet is not None:
                         row += f" · no progress {quiet:.0f}s"
@@ -1459,6 +1461,26 @@ class GatewaySlashCommandsMixin:
                 interrupt_reason=_INTERRUPT_REASON_STOP,
                 invalidation_reason="stop_command_handler",
             )
+            return EphemeralReply(t("gateway.stop.stopped"))
+
+        # Detached delegations outlive the foreground turn, so an otherwise
+        # idle session can still own queued/running work. /stop must cancel it.
+        try:
+            from tools.async_delegation import interrupt_for_session
+
+            _async_stopped = interrupt_for_session(
+                session_key=session_key,
+                parent_session_id=str(getattr(session_entry, "session_id", "") or ""),
+                reason="stop_command_idle",
+            )
+        except Exception:
+            logger.debug(
+                "Failed to interrupt idle-session delegations for %s",
+                session_key,
+                exc_info=True,
+            )
+            _async_stopped = 0
+        if _async_stopped:
             return EphemeralReply(t("gateway.stop.stopped"))
 
         # No run under the caller's own session key.  In a per-user thread

LuisMunozVillarreal/hermes-agent

gateway/slash_commands.py
@@ -435,6 +435,20 @@ class GatewaySlashCommandsMixin(
             await _stop(session_key, "stop_command_handler")
             return EphemeralReply(t("gateway.stop.stopped"))
 
+        # Detached work may remain after the foreground run has ended.
+        try:
+            from tools.async_delegation import interrupt_for_session
+            stopped = interrupt_for_session(
+                session_key=session_key,
+                parent_session_id=str(getattr(session_entry, "session_id", "") or ""),
+                reason="stop_command_idle",
+            )
+        except Exception:
+            logger.debug("Failed to interrupt idle-session delegations", exc_info=True)
+            stopped = 0
+        if stopped:
+            return EphemeralReply(t("gateway.stop.stopped"))
+
         # No run under the caller's own key. In a per-user thread (thread_sessions_per_user=True) a
         # run another user started lives under a different key, yet authorized users must still be
         # able to /stop it: fall back to sibling runs in this thread, gated on authorization.